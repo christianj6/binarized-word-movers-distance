0ecc038481ee94f7a96c66db9bb7a97420f150d5
Christian Johnson <christian.johnson@deepsight.de>
2020-08-08 15:17:19 +0200

    test setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bwmd',
+    version='0.0.1',
+    description='',
+    url='https://github.com/christianj6/binarized-word-movers-distance.git',
+    author='Christian Johnson',
+    author_email='chr.a.johnson@gmail.com',
+    license='unlicensed',
+    packages=['bwmd'],
+    zip_safe=False
+)
