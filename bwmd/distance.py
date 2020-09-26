a96651c3a759f1b3b31b34468b1a12f5e3f8c005
Christian Johnson <christian.johnson@deepsight.de>
2020-09-26 13:00:14 +0200

    remove unnecessary lines

--- bwmd/distance.py
+++ bwmd/distance.py
@@ -5,7 +5,8 @@ import dill
 from bwmd.compressor import load_vectors
 from scipy.spatial import distance as distance_scipy
 import annoy
-from gmpy2 import hamdist
+from gmpy2 import hamdist, to_binary
+import time
 
 # Set random seed.
 random.seed(42)
@@ -99,7 +100,6 @@ def build_kmeans_lookup_tables(vectors, I, path, save=True, vector_size=300):
             cache : dict
                 Precomputed values.
         '''
-        import time
         start = time.time()
         # Create a cache of distances to remove repeated calculations.
         cache = {}
@@ -139,24 +139,13 @@ def build_kmeans_lookup_tables(vectors, I, path, save=True, vector_size=300):
                     cluster = centroids[distances.index(min(distances))]
                     output[cluster].append(word)
 
-                # Adjust clusters to equal size.
-                len_cluster_1 = len(list(output.items())[0][1])
-                len_cluster_2 = len(list(output.items())[1][1])
-                larger_cluster = 0 if len_cluster_1 > len_cluster_2 else 1
+                # Update the lists.
                 both_clusters = [cluster_id for cluster_id, words in list(output.items()).copy()]
-                larger_cluster = both_clusters.pop(larger_cluster)
-                smaller_cluster = both_clusters[0]
-                sample_size = abs(len_cluster_1 - len_cluster_2)
-                sample_size = round(sample_size / 2)
-                for word in random.sample(output[larger_cluster], sample_size):
-                    # Logically we can just swap assignments because only two clusters.
-                    output[larger_cluster].remove(word)
-                    output[smaller_cluster].append(word)
-
-                new_partition.append([(word, vectors[word]) for word in output[larger_cluster]])
-                new_partition.append([(word, vectors[word]) for word in output[smaller_cluster]])
-                centroid_words.append(partition[larger_cluster][0])
-                centroid_words.append(partition[smaller_cluster][0])
+                a, b = both_clusters[0], both_clusters[1]
+                new_partition.append([(word, vectors[word]) for word in output[a]])
+                new_partition.append([(word, vectors[word]) for word in output[b]])
+                centroid_words.append(partition[a][0])
+                centroid_words.append(partition[b][0])
                 new_partitions += new_partition
 
             # Update partitions.
@@ -164,147 +153,17 @@ def build_kmeans_lookup_tables(vectors, I, path, save=True, vector_size=300):
 
         # List the centroids as initialization for kmeans clustering.
         centroids = [list(vectors.keys()).index(word) for word in centroid_words]
-        # Create a reverse mapping of words to centroids for the ANN speedup.
-        token_to_centroid = {token: centroid for partition, centroid in zip(partitions, centroids)
-                                for token, vector in partition}
 
         # for centroid, tokens in zip(centroid_words, partitions):
         #     print(centroid.upper())
         #     print([token for token, vector in tokens])
         #     print('\n\n')
 
+        # Format output.
         output = zip(centroids, [[word for word, vector in partition] for partition in partitions])
 
         end = time.time()
         print(end - start)
-        exit()
-
-
-        # Build annoy embedding space.
-        print('Building approximate nearest-neighbor space ...')
-        # Add all tokens from the dataset.
-        index_to_token = {}
-        token_to_index = {}
-        # Add all tokens from the dataset.
-        index_to_token = {}
-        token_to_index = {}
-        if vector_size == 300:
-            # Use cosine distance if real-valued vectors.
-            metric = 'angular'
-        else:
-            # Use hamming distance if binary vectors.
-            metric = 'hamming'
-        a = annoy.AnnoyIndex(vector_size, metric=metric)
-        for i, (token, embedding) in tqdm(enumerate(vectors.items())):
-            if vector_size != 300:
-                # Map the string values to integers.
-                embedding = list(map(lambda x: int(x), list(embedding.bin)))
-            else:
-                # Deconstruct the np array.
-                embedding = embedding[0].tolist()
-            a.add_item(i, embedding)
-            # Add item and keep track of the mapping.
-            index_to_token[i] = token
-            token_to_index[token] = i
-
-        # Build embedding space.
-        a.build(n_trees=100)
-
-        # TODO: Build dictionary of all nns for faster lookup?
-        # TODO: Determine the optimal n value for the ann search.
-
-        # Iterate through the dictionary values to cluster.
-        for i in tqdm(range(I - 1)):
-            # Output mapping cluster_id:[words] for each iteration.
-            output = {centroid: [] for centroid in centroids}
-            vector_space = list(vectors.items())
-            for j, (word, vector) in tqdm(enumerate(vector_space)):
-                # Compare only relevant centroids, per Cheng et al. (2017),
-                # ie, only centroids under regime of word nearest-neighbors.
-                nn = a.get_nns_by_item(j, n=50)
-                # Remove nn which are centroids themselves.
-                nn = [i for i in nn if i not in list(output.keys())]
-                relevant_centroids = set()
-                for word in [index_to_token[item] for item in nn]:
-                    try:
-                        relevant_centroids.add(token_to_centroid[word])
-                    except KeyError:
-                        pass
-
-                # relevant_centroids = set([token_to_centroid[word] for word in
-                                                        # [index_to_token[item] for item in nn]])
-                distances = []
-
-                # print(relevant_centroids)
-                # exit()
-
-
-        #         # for centroid in centroids:
-                for centroid in relevant_centroids:
-
-
-
-
-                    try:
-                        # Attempt to lookup in cache.
-                        distance = cache[word][vector_space[centroid][0]]
-                    except KeyError:
-                        # Otherwise compute hamming distance.
-                        distance = hamming(vector, vector_space[centroid][1])
-                        # Update the cache.
-                        cache[word] = {}
-                        cache[word][vector_space[centroid][0]] = distance
-
-                    distances.append(distance)
-
-                cluster = centroids[distances.index(min(distances))]
-                # Update outputs.
-                output[cluster].append(word)
-
-            # Determine mean point within each cluster.
-            new_output = {}
-            for cluster, words in tqdm(output.items()):
-                # print(len(words))
-                word_distances = []
-                for word1 in words:
-                    distances = []
-                    for word2 in words:
-                        try:
-                            # Try use cache distance.
-                            distance = cache[word1][word2]
-                        except KeyError:
-                            # Otherwise compute distance.
-                            distance = hamming(vectors[word1], vectors[word2])
-                            # Update cache.
-                            cache[word1][word2] = distance
-
-                        distances.append(distance)
-
-                    word_distances.append(sum(distances) / len(distances))
-
-                # print(word_distances)
-                try:
-                    mean = words[word_distances.index(min(word_distances))]
-                    # Update position of centroid
-                    centroid = token_to_index[mean]
-                    # Store new output info.
-                    new_output[centroid] = words
-                except ValueError:
-                    # Skip in cases where a cluster has become empty.
-                    new_output[cluster] = words
-
-            output = new_output
-            # Update the centroids.
-            centroids = list(output.keys())
-
-            # Update token_to_centroid.
-            token_to_centroid = {token: centroid for centroid, words in
-                                    list(output.items()) for token in words}
-
-        for centroid, words in output.items():
-            print('Cluster ', str(centroid), ': ', str(len(words)), ' points')
-            print(words)
-            print('\n\n\n')
 
         return output, cache
 
