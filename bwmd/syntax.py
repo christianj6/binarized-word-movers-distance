3cc68512c493aabc9bcb7956fbbd2c3b6363aebf
Christian Johnson <christian.johnson@deepsight.de>
2020-10-02 13:38:09 +0200

    draft for syntax distance table computation

---
+++ bwmd/syntax.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import dill
+
+# TODO: Create network graph of SpaCy dependencies, which
+# replicated the hierarchy present in the Stanford dependencies hierarchy.
+
+# TODO: For all pairs of dependencies, calculate the path length.
+
+# TODO: Normalize these lengths by the maximum hierarchy depth, ie make
+# a separate function for calculating the LCH score.
+
+# TODO: Convert the LCH score to a distance.
+
+# TODO: Save these distance in a lookup table in an appropriate res directory.
