f2d381026b750c9ff33265d8a3fae1d24458fcdf
Christian Johnson <christian.johnson@deepsight.de>
2020-10-28 19:01:31 +0100

    draft generic scoring function

--- test/test_triplets.py
+++ test/test_triplets.py
@@ -1,12 +1,68 @@
 import unittest
 from bwmd.compressor import load_vectors
 from bwmd.distance import convert_vectors_to_dict, BWMD
+import dill
 
 
-# TODO: Helper functions for triplets task.
-# TODO: All test cases must use all embeddings.
-# TODO: Generic function for running a test.
-# TODO: a and b should be more similar than b and c.
+# TODO: Documentation justifying the scoring method.
+
+def evaluate_triplets_task(model, dim, syntax, raw_hamming):
+    '''
+    Score a given model on the wikipedia triplets,
+    returning the test error as a percentage.
+
+    Parameters
+    ---------
+        model : str
+            Name of model.
+        dim : str
+            Dimension of model.
+        syntax : bool
+            Whether to use syntax
+            info in the evaluation.
+        raw_hamming : bool
+            Whether to just use
+            the raw hamming distances for the
+            calculations.
+
+    Returns
+    ---------
+        error : float
+            Test error as percentage
+            of tuples for which the incorrect
+            evaluation was made.
+    '''
+    # Load all the triplets.
+    corpus = []
+    for i in range(20_000):
+        try:
+            with open(f'res\\datasets\\triplets\\wikipedia-{i}', 'rb') as f:
+                corpus.append(dill.load(f))
+
+        except FileNotFoundError:
+            continue
+
+    # Initialize BWMD.
+    bwmd = BWMD(model, dim, with_syntax=syntax,
+                    raw_hamming=raw_hamming)
+
+    # List to store the errors.
+    score = []
+    for triplet in corpus:
+        # Split the texts.
+        triplet = list(map(lambda x: x.split(), triplet))
+        # Pairwise distances.
+        matrix = bwmd.pairwise(triplet)
+        # Conditional for scoring
+        if not matrix[0][1] < matrix[0][2] and matrix[1][0] < matrix[1][2]:
+            # Just use a binary scoring method.
+            score.append(0)
+        else:
+            score.append(1)
+
+    # Return simple average as percentage error.
+    return sum(score) / len(score)
+
 
 class TestCase(unittest.TestCase):
     '''
@@ -16,6 +72,7 @@ class TestCase(unittest.TestCase):
     def test_wikipedia_hamming_syntax(self):
         '''
         '''
+        # TODO: All test cases must use all embeddings.
         pass
 
     def test_wikipedia_hamming_no_syntax(self):
