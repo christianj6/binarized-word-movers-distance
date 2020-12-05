ddd986a29c5842caedf0b7ba361bcfe38e7998c0
Christian Johnson <christian.johnson@deepsight.de>
2020-12-05 09:27:31 +0100

    add packages to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,22 @@ setup(
     author_email='',
     license='unlicensed',
     package_dir={'': 'bwmd'},
-    install_requires=[]
+    install_requires=[
+        'nltk==3.5',
+        'numpy==1.18.5',
+        'tensorflow==2.3.0',
+        'tensorflow_probability==0.11.0',
+        'tqdm==4.47.0',
+        'gensim==3.8.3',
+        'bitstring==3.1.7',
+        'requests==2.24.0',
+        'spacy==2.3.2',
+        'networkx==2.5',
+        'matplotlib==3.3.0',
+        'pyemd==0.5.1',
+        'dill==0.3.2',
+        'scipy==1.4.1',
+        'beautifulsoup4==4.9.3',
+        'gmpy2==2.0.8',
+    ]
 )
