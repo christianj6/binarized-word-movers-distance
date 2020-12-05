26360b8ab71a19c55ff9c6cd963f29f5913cc8a4
Christian Johnson <christian.johnson@deepsight.de>
2020-12-05 10:10:54 +0100

    change url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='bwmd',
     version='0.0.1',
     description='Fast, robust text similarity with autoencoder compressed word embeddings.',
-    url='git+ssh://git@github.com/christianj6/binarized-word-movers-distance.git',
+    url='',
     author='Christian Johnson',
     author_email='',
     license='unlicensed',
